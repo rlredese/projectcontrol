46fe63d5700158ea8cd2db2b2d0fe8b0cb412aa0
Rodrigo Lipert <rodrigosl.redes@gmail.com>
2020-05-31 14:35:02 -0300

    add: task page

--- hour_controle/core/views.py
+++ hour_controle/core/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .models import task_register
 
 def home(request):
-    
+
+    #all_tasks = task_register.object.all()
+    #for task in all_tasks:
+        #total_time_task =+ task.time_task
+
     return render(request,'home.html')
 
 def task_register(request):
